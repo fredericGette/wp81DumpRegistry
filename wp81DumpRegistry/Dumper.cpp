32ff654bb24de11655a49e29d8bbef4bc361ba71
fredericGette <frederic.gette@gmail.com>
2022-08-11 22:44:12 +0200

    escape characters

--- wp81DumpRegistry/Dumper.cpp
+++ wp81DumpRegistry/Dumper.cpp
@@ -15,6 +15,21 @@ void debug(const char* format, ...)
 	va_end(args);
 }
 
+TCHAR* escape(TCHAR* buffer, DWORD bufferSize) {
+	int i;
+	TCHAR* dest = (TCHAR*)calloc(bufferSize * 2, sizeof(TCHAR));
+	TCHAR* ptr = dest;
+	for (i = 0; i<bufferSize; i++) {
+		TCHAR src = buffer[i];
+			if (src == '\\' || src == '\"') {
+				*ptr++ = '\\';
+			}
+			*ptr++ = src;
+	}
+	return dest;
+}
+
+// TODO: replace "/" by "//" in keys and values to be compliant with JSON spec.
 void Dump(HKEY rootKey, TCHAR *rootKeyName, boolean isFirst)
 {
 	if (!isFirst)
@@ -26,52 +41,52 @@ void Dump(HKEY rootKey, TCHAR *rootKeyName, boolean isFirst)
 
 	Win32Api win32Api;
 
-	TCHAR achClass[MAX_PATH] = TEXT("");  // buffer for class name 
-	DWORD cchClassName = 0; // size of class string
-	DWORD cSubKeys = 0; // number of subkeys 
-	DWORD cbMaxSubKey; // longest subkey size
-	DWORD cchMaxClass; // longest class string 
-	DWORD cValues; // number of values for key
-	DWORD cchMaxValue; // longest value name
-	DWORD cbMaxValueData; // longest value data
-	DWORD cbSecurityDescriptor; // size of security descriptor
-	FILETIME ftLastWriteTime; // last write time
+	TCHAR ClassName[MAX_PATH] = TEXT("");  // buffer for class name 
+	DWORD ClassNameSize = 0; // size of class string
+	DWORD NbSubKeys = 0; // number of subkeys 
+	DWORD MaxSubKeyNameLength; // longest subkey size
+	DWORD MaxClassNameLength; // longest class string 
+	DWORD NbValues; // number of values for key
+	DWORD MaxValueNameLength; // longest value name
+	DWORD MaxValueDataLength; // longest value data
+	DWORD SecurityDescriptorSize; // size of security descriptor
+	FILETIME LastWriteTime; // last write time
 
-	TCHAR achKey[MAX_KEY_LENGTH]; // buffer for subkey name
-	DWORD cbName = MAX_KEY_LENGTH; // size of name string
+	TCHAR SubKeyName[MAX_KEY_LENGTH]; // buffer for subkey name
+	DWORD SubKeyNameSize = MAX_KEY_LENGTH; // size of name string
 
-	TCHAR achValue[MAX_VALUE_NAME]; // buffer for value name
-	DWORD cchValue = MAX_VALUE_NAME; // size of name string
-	DWORD valueType;
+	TCHAR ValueName[MAX_VALUE_NAME]; // buffer for value name
+	DWORD ValueNameSize = MAX_VALUE_NAME; // size of name string
+	DWORD ValueType;
 
 
-	DWORD retCode = win32Api.RegQueryInfoKeyW(rootKey, achClass, &cchClassName, NULL, &cSubKeys, &cbMaxSubKey, &cchMaxClass, 
-		&cValues, &cchMaxValue, &cbMaxValueData, &cbSecurityDescriptor, &ftLastWriteTime);
+	DWORD retCode = win32Api.RegQueryInfoKeyW(rootKey, ClassName, &ClassNameSize, NULL, &NbSubKeys, &MaxSubKeyNameLength, &MaxClassNameLength, 
+		&NbValues, &MaxValueNameLength, &MaxValueDataLength, &SecurityDescriptorSize, &LastWriteTime);
 
-	PBYTE valueBuf = new BYTE[cbMaxValueData];
-	DWORD valueBufSize = cbMaxValueData;
+	PBYTE ValueData = new BYTE[MaxValueDataLength];
+	DWORD ValueDataSize = MaxValueDataLength;
 
-	if (cSubKeys)
+	if (NbSubKeys)
 	{
 		debug("\t,\"keys\":[\n");
 
-		for (DWORD i = 0; i<cSubKeys; i++)
+		for (DWORD i = 0; i<NbSubKeys; i++)
 		{
-			cbName = MAX_KEY_LENGTH;
+			SubKeyNameSize = MAX_KEY_LENGTH;
 			retCode = win32Api.RegEnumKeyExW(rootKey, i,
-				achKey,
-				&cbName,
+				SubKeyName,
+				&SubKeyNameSize,
 				NULL,
 				NULL,
 				NULL,
-				&ftLastWriteTime);
+				&LastWriteTime);
 			if (retCode == ERROR_SUCCESS)
 			{
 				HKEY subKey = {};
-				retCode = win32Api.RegOpenKeyExW(rootKey, achKey, 0, KEY_READ, &subKey);
+				retCode = win32Api.RegOpenKeyExW(rootKey, SubKeyName, 0, KEY_READ, &subKey);
 				if (retCode == ERROR_SUCCESS)
 				{
-					Dump(subKey, achKey, i==0);
+					Dump(subKey, SubKeyName, i==0);
 				}
 			}
 		}
@@ -79,21 +94,21 @@ void Dump(HKEY rootKey, TCHAR *rootKeyName, boolean isFirst)
 	}
 
 
-	if (cValues)
+	if (NbValues)
 	{
 		debug("\t,\"values\":[\n");
 
-		for (DWORD i = 0, retCode = ERROR_SUCCESS; i<cValues; i++)
+		for (DWORD i = 0, retCode = ERROR_SUCCESS; i<NbValues; i++)
 		{
-			cchValue = MAX_VALUE_NAME;
-			achValue[0] = '\0';
+			ValueNameSize = MAX_VALUE_NAME;
+			ValueName[0] = '\0';
 			retCode = win32Api.RegEnumValueW(rootKey, i,
-				achValue,
-				&cchValue,
+				ValueName,
+				&ValueNameSize,
 				NULL,
-				&valueType,
-				valueBuf, 
-				&valueBufSize);
+				&ValueType,
+				ValueData, 
+				&ValueDataSize);
 
 			if (retCode == ERROR_SUCCESS)
 			{
@@ -102,20 +117,22 @@ void Dump(HKEY rootKey, TCHAR *rootKeyName, boolean isFirst)
 					debug(",");
 				}
 				debug("{\n");
-				debug("\t\"name\":\""); OutputDebugString(achValue); debug("\"\n");
+				TCHAR* escapedValueName = escape((TCHAR*)ValueName, ValueNameSize);
+				debug("\t\"name\":\""); OutputDebugString(escapedValueName); debug("\"\n");
 				debug("\t,\"value_type\":");
 
-				if (REG_SZ == valueType)
+				if (REG_SZ == ValueType)
 				{
 					debug("\"REG_SZ\"\n");
-					debug("\t,\"value\":\"\"\n");
+					TCHAR* escapedValueData = escape((TCHAR*)ValueData, ValueDataSize);
+					debug("\t,\"value\":\""); OutputDebugString((LPCWSTR)escapedValueData); debug("\"\n");
 				} 
-				else if (REG_DWORD == valueType)
+				else if (REG_DWORD == ValueType)
 				{
 					debug("\"REG_DWORD\"\n");
-					debug("\t,\"value\":\"%08X\"\n", *(PDWORD)valueBuf);
+					debug("\t,\"value\":\"0x%08X\"\n", *(PDWORD)ValueData);
 				}
-				else if (REG_MULTI_SZ == valueType)
+				else if (REG_MULTI_SZ == ValueType)
 				{
 					debug("\"REG_MULTI_SZ\"\n");
 					debug("\t,\"value\":\"\"\n");
